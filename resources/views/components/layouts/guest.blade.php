2744c6fd599a8c6ca9332d052e7933e0756ca8a7
rabbitmaid <rabbitmaid@proton.me>
2025-03-06 17:44:51 +0100

    First Commit

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.guest.page>
+    {{ $slot }}
+</x-layouts.guest.page>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-layouts.guest>
+
+    <div class="py-20 px-5">
+        <h1 class="text-gray-900 dark:text-white text-5xl text-center">Edusphere</h1>
+    </div>
+
+
+
+
+</x-layouts.guest>
