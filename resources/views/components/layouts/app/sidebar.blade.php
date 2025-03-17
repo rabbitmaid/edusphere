4bc905ef95d4277add74d1c2a6e1715d9935fceb
rabbitmaid <rabbitmaid@proton.me>
2025-03-17 07:04:07 +0100

    Created the transactions table

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -72,8 +72,10 @@
             
             <flux:navlist variant="outline">
                 <flux:navlist.group heading="Records and Settings" class="grid">
+
+                    <flux:navlist.item icon="chart-bar-square" :href="route('admin.dashboard.settings')" :current="request()->routeIs('admin.dashboard.settings*')" class="mb-2" wire:navigate>{{ __('Transactions') }}</flux:navlist.item>
                  
-                        <flux:navlist.item icon="cog" :href="route('admin.dashboard.settings')" :current="request()->routeIs('admin.dashboard.settings*')" class="mb-2" wire:navigate>{{ __('System Settings') }}</flux:navlist.item>
+                    <flux:navlist.item icon="cog" :href="route('admin.dashboard.settings')" :current="request()->routeIs('admin.dashboard.settings*')" class="mb-2" wire:navigate>{{ __('System Settings') }}</flux:navlist.item>
  
            
                 </flux:navlist.group>
